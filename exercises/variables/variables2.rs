6d5f138715a8e7001527575b554417fe9ed1ec4e
manuelzander <manuel.zander@hotmail.de>
2021-05-06 15:09:59 +0000

    More exercises

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,13 +1,12 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    println!("Hello world");
+    let x: i32 = 11;
     if x == 10 {
-        println!("Ten!");
+        println!("TEN");
     } else {
-        println!("Not ten!");
+        println!("NOT TEN");
     }
 }
